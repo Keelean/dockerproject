c14887f479afbc95dea41fcc767a5d4220d81fe8
Kingsley Amaeze <cmkslee@gmail.com>
2023-02-23 16:48:18 +0100

    Added new CI/CD workflow

--- src/main/java/com/keelean/dockerproject/DockerprojectApplication.java
+++ src/main/java/com/keelean/dockerproject/DockerprojectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerprojectApplication {//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
+public class DockerprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerprojectApplication.class, args);
